9714eb83258b62a5dc7474a4199adf18af9360dc
sinanshamsudheen <carreersins@gmail.com>
2024-10-29 18:09:56 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,46 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+class calc extends JPanel implements ActionListener{
+    private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bmul,badd,bdiv,bsub,beql,bclr;
+    public calc(){
+        JFrame f=new JFrame("Calculator");
+        JPanel p=new JPanel();
+        p.setBounds(0,0,600,800);
+        
+        f.setLayout(null);
+        f.setSize(700,900);
+        f.setVisible(true);
+        JTextField t=new JTextField();
+        b1=new JButton("1");
+        b2=new JButton("2");
+        b3=new JButton("3");
+        b4=new JButton("4");
+        b5=new JButton("5");
+        b6=new JButton("6");
+        b6=new JButton("6");
+        b7=new JButton("7");
+        b8=new JButton("8");
+        b9=new JButton("9");
+        b0=new JButton("0");
+        badd=new JButton("+");
+        bmul=new JButton("*");
+        bdiv=new JButton("/");
+        bsub=new JButton("-");
+        beql=new JButton("=");
+        bclr=new JButton("Clear");
+
+        add(b1);
+       
+
+
+    }
+    public void actionPerformed(ActionEvent e){
+        
+    }
+}
+public class calculator{
+    public static void main(String[] args) {
+        calc c=new calc();
+    }
+}
