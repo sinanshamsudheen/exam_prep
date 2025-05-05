75b1be8fc77570404aa031cc227401d2afceff5d
sinanshamsudheen <carreersins@gmail.com>
2025-05-05 21:05:44 +0530

    today's

---
+++ Anjali.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+void main()
+{
+//     int n,sum=0;
+//     printf("Enter the value of n : ");
+//     scanf("%d",&n);
+
+
+//     for(int i=0;i<=n;i++){
+//     printf( "%d",i);
+//     }
+//     printf( "\n");
+
+//     for(int i=0;i<n;i++){
+//     printf( "%d",i);
+//     }
+//     printf( "\n");
+
+//     for(int i=0;i<n-1;i++){
+//     printf( "%d",i);
+//     }
+//     printf( "\n");
+
+//     for(int i=1;i<n-1;i++){
+//     printf( "%d",i);
+//     }
+
+
+
+// }
+int a,b;
+scanf("%d%d",&a,&b);
+
+int anjali=0;
+
+if(a>b){
+    anjali=1;
+}
+
+if(anjali==1){
+    printf("%d is greater",a);
+}
+else{
+    printf("%d is greater",b);
+}
+}
